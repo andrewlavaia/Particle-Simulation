bb3007d41dfd04d0ea532b61123a13a37c783d4e
Andrew L <alavaia@yahoo.com>
2018-04-26 14:46:40 -0400

    Added a collision system and event tracking

--- bouncingballs.py
+++ bouncingballs.py
@@ -5,26 +5,22 @@ priority queue to check for collisions
 
 import random
 import math
+import heapq
 from graphics import  *
 
 class Ball:
-    x, y = 0, 0         # position
-    vx, vy = 0, 0       # velocity
-    radius = 1
-    mass = 1
-    maxWidth = 1
-    maxHeight = 1
-    collisionCnt = 0   
-
     def __init__(self, x, y, vx, vy, r, windowWidth, windowHeight):
-        self.x = x
-        self.y = y
-        self.vx = vx
+        self.x = x                          # position
+        self.y = y                         
+        self.vx = vx                        # velocity  
         self.vy = vy
-        self.radius = r
-        self.maxWidth = windowWidth
-        self.maxHeight = windowHeight
-
+        self.radius = r                     # size of Ball in pixels
+        self.mass = 1                       # used for collision physics
+        self.maxWidth = windowWidth         # highest possible x position
+        self.maxHeight = windowHeight       # highest possible y position
+        self.collisionCnt = 0               # number of pending collisions
+ 
+    # Moves ball by time * speed
     def move(self, dt):
         # if collision with wall, reverse speed 
         if self.x + (self.vx * dt) < self.radius or self.x + (self.vx * dt) > self.maxWidth - self.radius:
@@ -36,6 +32,7 @@ class Ball:
         self.x = self.x + (self.vx * dt)
         self.y = self.y + (self.vy * dt)
 
+    # Calculates time until collision with another Ball
     def timeToHit(that):
         if self == that:
             return math.inf 
@@ -49,16 +46,16 @@ class Ball:
         
         # collision prediction
         # d = (Δv*Δr)^2 − (Δv*Δv)(Δr*Δr − σ^2)
-        dvdr = dx*dvx + dy*dvy;
+        dvdr = dx*dvx + dy*dvy
         if dvdr > 0: 
             return math.inf
-        dvdv = dvx*dvx + dvy*dvy;
-        drdr = dx*dx + dy*dy;
-        sigma = self.radius + that.radius;
-        d = (dvdr*dvdr) - dvdv * (drdr - sigma*sigma);
+        dvdv = dvx*dvx + dvy*dvy
+        drdr = dx*dx + dy*dy
+        sigma = self.radius + that.radius
+        d = (dvdr*dvdr) - dvdv * (drdr - sigma*sigma)
         if d < 0:
             return math.inf;
-        return -(dvdr + math.sqrt(d)) / dvdv;
+        return -(dvdr + math.sqrt(d)) / dvdv
 
     def bounceOff(that):
         dx = that.x - self.x
@@ -67,6 +64,9 @@ class Ball:
         dvy = that.vy - self.vy
         dvdr = dx*dvx + dy*dvy
         dist = self.radius + that.radius
+
+        # calculate impulse J
+        # J = 2 * mass[i] * mass[j] (Δv * Δr) / σ(mass[i] + mass[j])
         J = 2 * self.mass * that.mass * dvdr / ((self.mass + that.mass) * dist)
         Jx = J * dx / dist
         Jy = J * dy / dist
@@ -74,8 +74,49 @@ class Ball:
         self.vy += Jy / self.mass
         that.vx -= Jx / that.mass
         that.vy -= Jy / that.mass
+
+        # increase collision count
         self.collisionCnt = self.collisionCnt + 1
         that.collisionCnt = that.collisionCnt + 1
+
+class Event:
+    def __init__(self, t, a, b):
+        self.time = t    
+        self.a = a
+        self.b = b
+        self.countA = 0
+        self.countB = 0
+
+    def compareTo(that):
+        return self.time - that.time
+
+    def isValid():
+        pass
+
+class CollisionSystem:
+    pq = []             # priority queue    
+    t = 0.0             # simulation clock time 
+    balls = []          # List of balls
+
+    # Adds all predicted collision times with this object to priority queue
+    def predict(a):
+        if a == None:
+            return
+
+        # insert collision time with all other balls into priority queue
+        for ball in balls:
+            dt = a.timeToHit(ball)
+            heapq.heappush(pq, Event(t + dt, a, balls[i]))
+
+        # insert time to hit walls into priority queue
+
+    # inserts predicted collision times into priority queue
+    # and processes the events
+    def simulate():
+        for ball in balls:
+            predict(ball)
+        heapq.heappush(pq, Event(0, None, None)) # needed so pq is never empty
+
 
 def main():
     win = GraphWin('Bouncing Balls', 500, 500)
@@ -101,10 +142,10 @@ def main():
  
 
     # Simulation Loop
-    while win.checkMouse() == None:
+    while win.checkMouse() == None: 
         i = 0
         for ball in balls:
-            ball.move(1)
+            ball.move(2)
             circles[i].move(ball.x - circles[i].getCenter().getX(), ball.y - circles[i].getCenter().getY())
             i = i + 1
     
