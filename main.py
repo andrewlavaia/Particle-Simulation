c8138a0aa9440c950fe396b60fca07ee1e95bcae
Andrew L <alavaia@yahoo.com>
2018-06-27 19:26:56 -0400

    Simulation can now run using user inputs from the main menu

--- main.py
+++ main.py
@@ -15,53 +15,69 @@ dataMap = {}
 config_flag = 0
 
 def load_config(file_string):
-    # load particle options
     with open(file_string) as f:
-        # use safe_load instead load
         dataMap = yaml.safe_load(f)
     return dataMap
 
-def set_config(input_1):
-    data = dict(
-        particles = dict(
-            group1 = dict(
-                n = 10,
-                color = 'black',
-                radius = 5.0,
-                mass = 1.0,
-                shape = "Circle",
-                width = 10.0,
-                height = 10.0 
-            ),
-            group2 = dict(                
-                n = 1,
-                color = 'green',
-                radius = 50.0,
-                mass = 20.0,
-                shape = "Circle",
-                width = 10.0,
-                height = 10.0
-            )
-        )
-    )
-
+def set_config(data):
     with open('config.yml', 'w') as outfile:
         yaml.safe_dump(data, outfile, default_flow_style=False)
-    
     global config_flag 
     config_flag = 1
+
+def create_particle_data(**kwargs):
+    data = {'particles': { } }
+    data.update( {'particles': kwargs} )
+    return data
 
 def main_menu():
     window.clear()
     window.setBackground('white')
-    txt_1_particles = Text(Point(window.width/2.0 - 200.0, window.height/2.0 - 50.0), 
+
+    # TODO 
+    # move all labels and input boxes into their own class that can
+    # be cloned, moved, and inserted anywhere on window
+    t1_n = Text(Point(window.width/2.0 - 200.0, window.height/2.0 - 200.0), 
             '# of particles: ')
-    txt_1_particles.setSize(24)
-    txt_1_particles.draw(window)
-    input_1_particles = Entry(Point(window.width/2.0 + 200.0, window.height/2.0 - 50.0), 4)
-    input_1_particles.setSize(24)
-    input_1_particles.setText(40)
-    input_1_particles.draw(window)
+    g1_n = Entry(Point(window.width/2.0 + 100.0, window.height/2.0 - 200.0), 4)
+    g1_n.setText(40)
+
+    t1_color = t1_n.clone()
+    t1_color.setText('color: ')
+    t1_color.move(0, 50)
+    g1_color = g1_n.clone()
+    g1_color.setText('black')
+    g1_color.width = 10
+    g1_color.move(0, 50)
+
+    t1_r = t1_n.clone()
+    t1_r.setText('radius: ')
+    t1_r.move(0, 100)
+    g1_r = g1_n.clone()
+    g1_r.setText(5.0)
+    g1_r.width = 4
+    g1_r.move(0, 100)
+
+    t1_m = t1_n.clone()
+    t1_m.setText('mass: ')
+    t1_m.move(0, 150)
+    g1_m = g1_n.clone()
+    g1_m.setText(1.0)
+    g1_m.width = 4
+    g1_m.move(0, 150)
+    
+    t1_n.draw(window)
+    t1_color.draw(window)
+    t1_r.draw(window)
+    t1_m.draw(window)
+
+    g1_n.draw(window)
+    g1_color.draw(window)
+    g1_r.draw(window)
+    g1_m.draw(window)
+
+    # TODO 
+    # validate inputs
 
     btn = Button(window, Point(window.width/2.0, window.height/2.0 + 100.0), 
             200, 100, 'Run Simulation')
@@ -71,10 +87,34 @@ def main_menu():
         last_clicked_pt = window.getMouse()
         if last_clicked_pt is not None:
             if btn.clicked(last_clicked_pt):
-                set_config('x')
+                kwargs = {
+                    'group1': {
+                        'n': g1_n.getText(),
+                        'color': g1_color.getText(),
+                        'radius': g1_r.getText(),
+                        'mass': g1_m.getText(),
+                        'shape': 'Circle',
+                        'width': float(g1_r.getText()) * 2,
+                        'height': float(g1_r.getText()) * 2
+                    }
+                }
+                
+                # group2 = {
+                #     'n': 2,
+                #     'color': 'green',
+                #     'radius': 30.0,
+                #     'mass': 10.0,
+                #     'shape': 'Circle',
+                #     'width': 10.0,
+                #     'height': 10.0
+                # }
+
+                # kwargs = {'group1': group1}
+                data = create_particle_data(**kwargs)
+                set_config(data)
                 main()
-            else:
-                print('not in button')
+            # else:
+                # print('not in button')
 
 def main():
     if not config_flag:
